65ebd0b2f1abaac0a31cfdbdb4aa471d557b9f39
AndyLinxies <linxies@gmail.com>
2021-05-11 16:34:45 +0200

    part2 ex1

---
+++ main.js
@@ -0,0 +1,14 @@
+//Ex1
+let ex1 = document.querySelector('p.tartine').innerText
+console.log(ex1);
+
+//Ex2
+let ex2 = document.querySelector('p#toast').innerText
+console.log(ex2);
+
+//Ex3
+let ex3 = document.querySelectorAll('.tartine')[1]
+console.log(ex3);
+
+
+
